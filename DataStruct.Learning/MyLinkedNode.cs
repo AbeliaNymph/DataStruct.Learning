8b7b4dbdd9882766fdf980efd61dfdaff4e0a52a
顾天皓 <gutianhao@wondersgroup.com>
2024-03-06 15:04:19 +0800

    init

---
+++ DataStruct.Learning/MyLinkedNode.cs
@@ -0,0 +1,8 @@
+﻿namespace DataStruct.Learning;
+
+public class MyLinkedNode<T>(T value)
+{
+    public T Value { get; set; } = value;
+
+    public MyLinkedNode<T>? Next { get; set; }
+}
---
+++ DataStruct.UnitTest/MyLinkedNodeTest.cs
@@ -0,0 +1,18 @@
+﻿using DataStruct.Learning;
+
+namespace DataStruct.UnitTest;
+
+[TestClass]
+public class MyLinkedNodeTest
+{
+
+    [TestMethod]
+    public void OneElementLinkedList_ToString_DisplayOneElement()
+    {
+        MyLinkedNode<int> obtained = new(1);
+
+        string expected = "Node(1) -> None";
+
+        Assert.AreEqual(expected, obtained.ToString());
+    }
+}
