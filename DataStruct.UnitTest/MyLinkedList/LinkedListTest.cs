b796fc6ce2020b52b983a402abca0f141cb34e5b
顾天皓 <gutianhao@wondersgroup.com>
2024-03-06 11:34:38 +0800

    init

---
+++ DataStruct.UnitTest/MyLinkedList/LinkedListTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using DataStruct.Learning;
+
+namespace DataStruct.UnitTest;
+
+[TestClass]
+public class LinkedListTest
+{
+
+    [TestMethod]
+    public void OneTwoThree_Append_OneConnectTwoConnectThreeConnectNull()
+    {
+        MyLinkedList<int> ll = new();
+
+        ll.Append(1);
+        ll.Append(2);
+        ll.Append(3);
+
+        string expected = "Root -> Node(1) -> Node(2) -> Node(3) -> Null";
+
+        Assert.AreEqual(expected, ll.ToString());
+
+    }
+
+    [TestMethod]
+    public void NoElement_ToString_isEmptyNode()
+    {
+        MyLinkedList<int> ll = new();
+
+        Assert.AreEqual("Root -> Null", ll.ToString());
+    }
+}
---
+++ DataStruct.UnitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace DataStruct.UnitTest;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
