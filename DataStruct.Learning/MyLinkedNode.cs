b0cdfaaa9606bbc60fbd05b3759bdd3d8fbf75b5
顾天皓 <gutianhao@wondersgroup.com>
2024-03-08 09:19:30 +0800

    init

--- DataStruct.Learning/MyLinkedNode.cs
+++ DataStruct.Learning/MyLinkedNode.cs
@@ -1,8 +1,36 @@
-﻿namespace DataStruct.Learning;
+﻿
+
+namespace DataStruct.Learning;
 
 public class MyLinkedNode<T>(T value)
 {
     public T Value { get; set; } = value;
 
     public MyLinkedNode<T>? Next { get; set; }
+
+    public override string ToString()
+    {
+        return $"Node({Value}) -> {(Next == null ? "None" : Next)}";
+    }
+
+    public void Append(T value)
+    {
+        MyLinkedNode<T> last = GetLastNode(this);
+        last.Next = new(value);
+    }
+
+    private MyLinkedNode<T> GetLastNode(MyLinkedNode<T> myLinkedNode)
+    {
+        if (myLinkedNode.HasNext())
+        {
+            return GetLastNode(myLinkedNode.Next!);
+        }
+
+        return myLinkedNode;
+    }
+
+    private bool HasNext()
+    {
+        return Next != null;
+    }
 }
--- DataStruct.UnitTest/MyLinkedNodeTest.cs
+++ DataStruct.UnitTest/MyLinkedNodeTest.cs
@@ -5,13 +5,38 @@ namespace DataStruct.UnitTest;
 [TestClass]
 public class MyLinkedNodeTest
 {
-
     [TestMethod]
     public void OneElementLinkedList_ToString_DisplayOneElement()
     {
         MyLinkedNode<int> obtained = new(1);
 
         string expected = "Node(1) -> None";
+
+        Assert.AreEqual(expected, obtained.ToString());
+    }
+
+    [TestMethod]
+    public void OneAndTwoElementLinkedList_Append_OneTwoThree()
+    {
+        MyLinkedNode<int> obtained = new(1);
+
+        obtained.Append(2);
+
+        string expected = "Node(1) -> Node(2) -> None";
+
+        Assert.AreEqual(expected, obtained.ToString());
+    }
+
+    [TestMethod]
+    public void RangeOneToThreeElementLinkedList_Append_RangeOneToFour()
+    {
+        MyLinkedNode<int> obtained = new(1);
+
+        obtained.Append(2);
+        obtained.Append(3);
+        obtained.Append(4);
+
+        string expected = "Node(1) -> Node(2) -> Node(3) -> Node(4) -> None";
 
         Assert.AreEqual(expected, obtained.ToString());
     }
