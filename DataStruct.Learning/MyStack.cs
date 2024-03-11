efca9930b5047862114e2e76682d897dff03fc32
顾天皓 <gutianhao@wondersgroup.com>
2024-03-11 17:52:24 +0800

    init

---
+++ DataStruct.Learning/MyStack.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace DataStruct.Learning;
+
+public class MyStack<T>
+{
+    private readonly List<T> _instance = [];
+
+    private int? _topIndex = -1;
+
+    public MyStack(T value)
+    {   _topIndex += 1;
+        _instance.Add(value);
+    }
+
+    public MyStack()
+    {
+
+    }
+
+    public override string ToString()
+    {
+        StringBuilder stringBuilder = new();
+
+        foreach (T item in _instance)
+        {
+            stringBuilder.Append($"{item} ");
+        }
+
+        stringBuilder.Append(']');
+
+        return stringBuilder.ToString();
+    }
+
+    public void Push(T value)
+    {
+        _topIndex += 1;
+        _instance.Add(value);
+    }
+}
---
+++ DataStruct.UnitTest/StackTest.cs
@@ -0,0 +1,47 @@
+﻿using DataStruct.Learning;
+
+namespace DataStruct.UnitTest;
+
+[TestClass]
+public class StackTest
+{
+
+    [TestMethod]
+    public void EmptyStack_Create_IsEmpty()
+    {
+        MyStack<int> obtained = new();
+
+        string expected = "]";
+
+        Assert.AreEqual(expected, obtained.ToString());
+
+    }
+
+    [TestMethod]
+    public void OneElement_ToString_IsOneElement()
+    {
+        MyStack<int> obtained = new(1);
+
+        string expected = "1 ]";
+
+        Assert.AreEqual(expected, obtained.ToString());
+    }
+
+    [TestMethod]
+    public void EmptyElement_Push_OneElement()
+    {
+        MyStack<int> obtained = new();
+
+        obtained.Push(1);
+
+        string expected = "1 ]";
+
+        Assert.AreEqual(expected, obtained.ToString()); 
+    }
+
+    [TestMethod]
+    public void EmptyElement_Pop_ThrowInvalidOperationException()
+    {
+        
+    }
+}
